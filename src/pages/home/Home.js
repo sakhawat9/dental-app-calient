f47bc51a99c92d0de21bf4f45a613d065105e668
sakhawat9 <sakhawathossain7969@gmail.com>
2022-01-12 08:32:57 +0600

    [ADD] hospital area

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Hero from "../../components/Hero";
+import HospitalArea from "../../components/HospitalArea";
 
 const Home = () => {
   return (
     <div>
       <Hero />
+      <HospitalArea />
     </div>
   );
 };
