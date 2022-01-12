26cb8b69516403e63d708432e97799db08a97fb9
sakhawat9 <sakhawathossain7969@gmail.com>
2022-01-12 21:53:40 +0600

    [ADD] special features

---
+++ src/components/SpecialFeatures.js
@@ -0,0 +1,94 @@
+import React from "react";
+
+const SpecialFeatures = () => {
+  return (
+    <div className="elementor-widget-wrap section-padding">
+      <div className="elementor-element elementor-element-a12c81b elementor-widget elementor-widget-Rola_Section">
+        <div className="elementor-widget-container">
+          <div className="container flex justify-center">
+            <div className="w-3/5 section-title">
+              <div className="">
+                <span className="subtitle"># OUR SPECIAL FEATURES</span>
+              </div>
+              <h2>
+                Features That You Will Love <b>Us and Enjoy Specialized</b> Care
+              </h2>
+              <p></p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="container">
+        <div className="grid grid-cols-12 gap-4">
+          <div className="col-span-6 md:col-span-3">
+            <div className="single-core-features">
+              <div className="title">
+                <i className="flaticon-hospitalisation"></i>
+                <h3>
+                  <a href="https://themes.hibootstrap.com/rola/service-post/patient-care/">
+                    Patient Care{" "}
+                  </a>
+                </h3>
+              </div>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipiscing elit sed do
+                eiusmod tempor
+              </p>
+            </div>
+          </div>
+          <div className="col-span-6 md:col-span-3">
+            <div className="single-core-features">
+              <div className="title">
+                <i className="flaticon-diamond"></i>
+                <h3>
+                  <a href="https://themes.hibootstrap.com/rola/service-post/experience/">
+                    Experience{" "}
+                  </a>
+                </h3>
+              </div>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipiscing elit sed do
+                eiusmod tempor
+              </p>
+            </div>
+          </div>
+          <div className="col-span-6 md:col-span-3">
+            <div className="single-core-features">
+              <div className="title">
+                <i className="flaticon-dental-care"></i>
+                <h3>
+                  <a href="https://themes.hibootstrap.com/rola/service-post/certified-dentists/">
+                    Certified Dentists{" "}
+                  </a>
+                </h3>
+              </div>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipiscing elit sed do
+                eiusmod tempor
+              </p>
+            </div>
+          </div>
+          <div className="col-span-6 md:col-span-3">
+            <div className="single-core-features">
+              <div className="title">
+                <i className="flaticon-dental-care"></i>
+                <h3>
+                  <a href="https://themes.hibootstrap.com/rola/service-post/quality-brackets/">
+                    Quality Brackets{" "}
+                  </a>
+                </h3>
+              </div>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipiscing elit sed do
+                eiusmod tempor
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SpecialFeatures;
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -2,6 +2,7 @@ import React from "react";
 import Hero from "../../components/Hero";
 import HospitalArea from "../../components/HospitalArea";
 import Services from "../../components/Services";
+import SpecialFeatures from "../../components/SpecialFeatures";
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <Hero />
       <Services />
       <HospitalArea />
+      <SpecialFeatures />
     </div>
   );
 };
