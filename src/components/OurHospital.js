bcf0db38f11e1f7ad1ad885abda5cfd1ac4eb176
habibshrabon <snhabib777@gmail.com>
2022-01-16 22:17:48 +0600

    some data set

--- src/components/OurHospital.js
+++ src/components/OurHospital.js
@@ -5,25 +5,25 @@ const OurHospital = () => {
     <div className="our-hospital">
       <div className="our-hospital__container">
         <div className="lg:col-span-6 md:col-span-12">
-          <div className="our-hospital__about-image">
+          <div className="our-hospital__about-image px-5">
             <img
               src="https://themes.hibootstrap.com/rola/wp-content/uploads/2021/10/about-1.jpg"
               alt=""
             />
           </div>
         </div>
-        <div className="lg:col-span-6 md:col-span-12">
+        <div className="lg:col-span-6 md:col-span-12 px-5">
           <span className="subtitle"># ABOUT OUR HOSPITAL</span>
-          <h2>
+          <h2 className="py-5">
             Our Patients Are <b>Our Priority, We Offer</b> Quality Dental
             Services
           </h2>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat,
-            dolorum accusantium obcaecati veniam voluptas soluta autem error
-            optio vero reiciendis?
+          <p className="pb-5">
+            Lorem ipsum dolor sit amet consectetur adipiscing elit sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. ut enim
+            ad minim veniam quis nostrud exercitation ullamco laboris.
           </p>
-          <div className="our-hospital__container">
+          <div className="grid grid-cols-12 gap-4">
             <div className="lg:col-span-6 md:col-span-12">
               <ul className="about-list">
                 <li>Complete Crown</li>
