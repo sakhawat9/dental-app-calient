b87a7c9fa266b2d685a1a2e6cc35342ca261edf3
sakhawat9 <sakhawathossain7969@gmail.com>
2022-01-21 18:12:04 +0600

    [ADD] review

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -2,6 +2,7 @@ import React from "react";
 import Features from "../../common/Features";
 import Hero from "../../components/Hero";
 import HospitalArea from "../../components/HospitalArea";
+import Review from "../../components/Review";
 import Services from "../../components/Services";
 import SpecialFeatures from "../../components/SpecialFeatures";
 
@@ -13,6 +14,7 @@ const Home = () => {
       <Services />
       <HospitalArea />
       <SpecialFeatures />
+      <Review />
     </div>
   );
 };
