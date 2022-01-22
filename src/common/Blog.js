78c88443b9b1f935d716c2f28d1ca919c05dfbbf
sakhawat9 <sakhawathossain7969@gmail.com>
2022-01-22 09:40:35 +0600

    [ADD] Blog section

---
+++ src/common/Blog.js
@@ -0,0 +1,35 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from "react";
+
+const Blog = ({ blogData }) => {
+  const { img, title, subtitle } = blogData;
+  return (
+    <div className="lg:col-span-4 md:col-span-6 col-span-12">
+      <div className="single-blog-card">
+        <div className="single-blog-card__image">
+          <a href="#">
+            <img src={img} alt={title} />
+          </a>
+        </div>
+        <div className="single-blog-card__content">
+          <ul className="entry-meta d-flex justify-content-between align-items-center">
+            <li>
+              <a href="#">{subtitle}</a>
+            </li>
+            <li>
+              <i className="bx bx-calendar"></i> July 31, 2021
+            </li>
+          </ul>
+          <h3>
+            <a href="#">{title}</a>
+          </h3>
+          <a href="#" className="blog-btn">
+            Read More
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Blog;
