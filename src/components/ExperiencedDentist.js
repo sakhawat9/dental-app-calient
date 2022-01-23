9268d257c42d6095865f4415f98b83711cd46758
monirul-fahad <immonirulfahad@gmail.com>
2022-01-23 12:49:52 +0600

    curossol added

--- src/components/ExperiencedDentist.js
+++ src/components/ExperiencedDentist.js
@@ -1,4 +1,9 @@
+/* eslint-disable jsx-a11y/img-redundant-alt */
 import React from "react";
+// import { AiOutlineStar } from "react-icons/ai";
+import { FaLongArrowAltLeft, FaLongArrowAltRight } from "react-icons/fa";
+// import { IoMdQuote } from "react-icons/io";
+import Slider from "react-slick";
 
 const doctorsData = [
   {
@@ -33,6 +38,62 @@ const doctorsData = [
   },
 ];
 
+const PreviousBtn = (props) => {
+  const { className, onClick } = props;
+  return (
+    <div className={className} onClick={onClick}>
+      <FaLongArrowAltLeft className="p-2 text-4xl font-extrabold text-white rounded-full shadow-lg" />
+    </div>
+  );
+};
+const NextBtn = (props) => {
+  const { className, onClick } = props;
+  return (
+    <div className={className} onClick={onClick}>
+      <FaLongArrowAltRight className="p-2 text-4xl text-white rounded-full shadow-lg" />
+    </div>
+  );
+};
+
+const carouselProperties = {
+  prevArrow: <PreviousBtn />,
+  nextArrow: <NextBtn />,
+  slidesToShow: 3,
+  centerMode: true,
+  centerPadding: "200px",
+  responsive: [
+    {
+      breakpoint: 512,
+      settings: {
+        slidesToShow: 1,
+        centerMode: false,
+      },
+    },
+    {
+      breakpoint: 769,
+      settings: {
+        slidesToShow: 2,
+        centerMode: false,
+      },
+    },
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        centerMode: false,
+      },
+    },
+    {
+      breakpoint: 2000,
+      settings: {
+        slidesToShow: 4,
+        centerMode: false,
+        slidesToScroll: 1,
+      },
+    },
+  ],
+};
+
 const ExperiencedDentist = () => {
   return (
     <div className="experienced-dentist">
@@ -45,6 +106,35 @@ const ExperiencedDentist = () => {
           <p></p>
         </div>
       </div>
+      <div className="container mx-auto carousel section-padding">
+        <Slider {...carouselProperties}>
+          {doctorsData.map((item, index) => (
+            <Card key={index} item={item} />
+          ))}
+        </Slider>
+      </div>
+    </div>
+  );
+};
+
+const Card = ({ item }) => {
+  return (
+    // <div className="single-review-item">
+    //   <div className="single-review-item__info">
+    //     <img src={item.img} alt="image" />
+    //     <h3>Eachann Jhon</h3>
+    //     <span>Founder</span>
+    //   </div>
+    //   <div className="single-review-item__quote-icon">
+    //     <ul>
+    //       <li>
+    //         <IoMdQuote />
+    //       </li>
+    //     </ul>
+    //   </div>
+    // </div>
+    <div className="container">
+      <img src={item.img} alt="" />
     </div>
   );
 };
